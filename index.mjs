79963bf66931efc2b4779130141f6315608fc094
= <=>
2022-01-02 20:42:05 +0545

    First commit

---
+++ index.mjs
@@ -0,0 +1,12 @@
+// console.log('Hello World')
+
+// // 1. Common JS
+// const util = require('./util');
+
+// console.log(util)
+
+// 2. module es6
+
+import fs from 'fs'
+
+console.log(fs.readFileSync('index.mjs', 'utf8'))
---
+++ util.js
@@ -0,0 +1,5 @@
+function add(a, b){
+    return a+b
+}
+
+exports.a = 100
