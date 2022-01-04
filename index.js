b71878811b588f456c710b38cec00db4b7c00808
= <=>
2022-01-04 20:48:29 +0545

    writefilesync flag passed

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const {readFileSync, writeFileSync} = require('fs')
 
 const htmlText = readFileSync('./public/index.html', 'utf-8')
 
-writeFileSync('./public/hello.txt', 'Hello World')
+writeFileSync('./public/hello.txt', 'Hello World', {flag: 'a'})
 
 console.log(htmlText);
