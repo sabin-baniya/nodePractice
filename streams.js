f00ded2bd35a1bdb905527bb62be7db58318e807
Sabin Baniya <dummymail@tutanota.de>
2022-01-07 13:45:34 +0545

    File streams in server

--- streams.js
+++ streams.js
@@ -1,7 +1,21 @@
 const {createReadStream, readFileSync} = require('fs')
+const http = require('http')
 
 const stream = createReadStream('./big-file.txt', {encoding: 'utf8'})
 
-stream.on('data', (chunk ) => {
-    console.log(chunk)
-})
+// stream.on('data', (chunk ) => {
+//     console.log(chunk)
+// })
+
+http.createServer((req,res) => {
+    // const text = readFileSync('./big-file.txt', 'utf8');
+    
+    const stream = createReadStream('../big-file.txt', 'utf8');
+    stream.on('open', () => {
+        stream.pipe(res)
+    })
+    stream.on('error', (err) => {
+        // res.write(err)
+    })
+    // res.end(text)
+}).listen(5000)
