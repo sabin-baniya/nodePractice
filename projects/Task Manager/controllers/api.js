6a0dde118abea84616ce08433ab08f6d8937f534
Sabin Baniya <dummymail@tutanota.de>
2022-01-08 19:13:15 +0545

    Get put delete

--- projects/Task Manager/controllers/api.js
+++ projects/Task Manager/controllers/api.js
@@ -1,24 +1,56 @@
 const Tasks = require('../models/schema')
 
-const getTasks = (req, res) => {
+const getTasks = async (req, res) => {
+    try{
+        const tasks = await Tasks.find({})
+        res.status(200).json({tasks})
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
     
 }
 
 const postTasks = async (req, res) => {
-    const task = await Tasks.create(req.body)
-    res.status(201).send(task)
+    try {
+        const task = await Tasks.create(req.body)
+        res.status(201).send(task)
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
-const getSingleTask = (req, res) => {
-    res.send('Hello there')
+const getSingleTask = async (req, res) => {
+    try {
+        const {id:TaskID} = req.params
+        const task = await Tasks.findOne({_id:TaskID})
+
+        if(!task){
+            return res.status(404).json({msg: `No such task with id ${id}`})
+        }
+
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
 const patchTask = (req, res) => {
     res.send('Hello there')
 }
 
-const deleteTask = (req, res) => {
-    res.send('Hello there')
+const deleteTask = async (req, res) => {
+    try {
+        const {id:TaskID} = req.params
+        const task = await Tasks.findOneAndDelete({_id:TaskID})
+
+        if(!task){
+            return res.status(404).json({msg: `No such task with id ${id}`})
+        }
+
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error}) 
+    } 
 }
 
 module.exports = {
