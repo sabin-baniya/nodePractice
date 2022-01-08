d9696979427e838eb1d4c9464b18040f26874c09
Sabin Baniya <dummymail@tutanota.de>
2022-01-08 18:26:53 +0545

    Schema validation

--- projects/Task Manager/models/schema.js
+++ projects/Task Manager/models/schema.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'Is Required'],
+        maxlength: [20, 'Cannot exceed limit of 20 char'],
+        trim: true 
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
 module.exports = mongoose.model('Tasks', TaskSchema)
